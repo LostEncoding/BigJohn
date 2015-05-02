83914862c894ee98f34240cbeb6774907e737dc7
Anamor7 <tburkhart@unomaha.edu>
2015-05-02 17:20:01 -0500

    Finished Sandwich Randomization
    Added Condiments

--- Assets/Scripts/SandwichController.cs
+++ Assets/Scripts/SandwichController.cs
@@ -12,7 +12,7 @@ public class SandwichController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		print (SandwichName);
+		print(SandwichName);
 	}
 
 	void createSandwich(){
@@ -20,34 +20,88 @@ public class SandwichController : MonoBehaviour {
 		string Meat = randomizeMeat ();
 		string Cheese = randomizeCheese ();
 		string Sauce = randomizeSauce ();
-		SandwichName = Bread + " " + Meat + " " + Cheese + " " + Sauce;
+		string Extra = randomizeExtra ();
+		SandwichName = Meat + " on " + Bread + " with " + Cheese + ", " + Sauce + Extra;
 	}
 
 	public string randomizeBread(){
-		string[] Bread = {"White", "Wheat", "Rye", "Marble"};
+		string[] Bread = {"White Bread", "Wheat Bread", "Rye Bread", "Marble Bread"};
 		int breadInt = 0;
-		breadInt = Random.Range(0,3);
+		breadInt = Random.Range(0,4);
 		return Bread[breadInt];
 	}
 
 	public string randomizeMeat(){
-		string[] Meat = {"Turkey", "Chicken", "Beef", "Baon", "Pork"};
+		string[] Meat = {"Turkey", "Chicken", "Beef", "Bacon", "Pork"};
 		int meatInt = 0;
-		meatInt = Random.Range (0,4);
+		meatInt = Random.Range (0,5);
 		return Meat[meatInt];
 	}
 
 	public string randomizeCheese(){
-		string[] Cheese = {"American", "Provolone", "Pepperjack", "Cheddar", "Gouda"};
+		string[] Cheese = {"American Cheese", "Provolone Cheese", "Pepperjack Cheese", "Cheddar Cheese", "Gouda Cheese"};
 		int cheeseInt = 0;
-		cheeseInt = Random.Range (0,4);
+		cheeseInt = Random.Range (0,5);
 		return Cheese[cheeseInt];
 	}
 
 	public string randomizeSauce(){
 		string[] Sauce = {"Mayonnaise", "Mustard", "Barbecue Sauce", "Oil", "Vinegar"};
 		int sauceInt = 0;
-		sauceInt = Random.Range (0,4);
+		sauceInt = Random.Range (0,5);
 		return Sauce[sauceInt];
+	}
+
+	public string randomizeExtra(){
+		string Extra = "";
+		string Extra1 = "Lettuce";
+		string Extra2 = "Tomato";
+		string Extra3 = "Pickles";
+		string Extra4 = "Olives";
+		string[] Peppers = {"Green Peppers", "Yellow Peppers", "Jalapeno Peppers"};
+		int extraInt1 = 0;
+		int extraInt2 = 0;
+		int extraInt3 = 0;
+		int extraInt4 = 0;
+		int pepperInt = 0;
+		int pepperInt2 = 0;
+		extraInt1 = Random.Range (0, 2);
+		extraInt2 = Random.Range (0, 2);
+		extraInt3 = Random.Range (0, 2);
+		extraInt4 = Random.Range (0, 2);
+		pepperInt = Random.Range (0, 2);
+		pepperInt2 = Random.Range (0, 3);
+		if (extraInt1 == 0) {
+			if (!(extraInt2 != 0 || extraInt3 != 0 || extraInt4 != 0 || pepperInt != 0)){
+				Extra = Extra + ", and " + Extra1;
+			}else{
+				Extra = Extra + ", " + Extra1;
+			}
+		}
+		if (extraInt2 == 1) {
+			if (!(extraInt3 != 0 || extraInt4 != 0 || pepperInt != 0)){
+				Extra = Extra + ", and " + Extra2;
+			}else{
+				Extra = Extra + ", " + Extra2;
+			}
+		}
+		if (extraInt3 == 1) {
+			if (!(extraInt4 != 0 || pepperInt != 0)){
+				Extra = Extra + ", and " + Extra3;
+			}else{
+				Extra = Extra + ", " + Extra3;
+			}
+		}
+		if (extraInt4 == 1) {
+			if (!(pepperInt != 0)){
+				Extra = Extra + ", and " + Extra4;
+			}else{
+				Extra = Extra + ", " + Extra4;
+			}
+		}
+		if (pepperInt == 1) {
+			Extra = Extra + ", and " + Peppers [pepperInt2];
+		}
+		return Extra;
 	}
 }
